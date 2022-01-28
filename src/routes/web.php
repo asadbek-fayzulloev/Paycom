d9dc206bce23dddd49a7b4df6c7060210f87a81f
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-01-28 17:36:32 +0500

    Update routes

---
+++ src/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+use Asadbek\Paycom\Http\Controller\PaycomController;
+
+Route::group([
+    'middleware' => 'web',
+    'prefix' => 'paycom',
+    'as' => 'paycom.',
+    'namespace' => 'Asadbek\Paycom\Http\Controllers'
+], function () {
+    Route::post('paycom', [PaycomController::class,'index'])->name('paycom');
+});
