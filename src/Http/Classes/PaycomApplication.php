f6b8178f4a534eb24f793e58c1b3244da0cd5c14
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-04 16:01:15 +0500

    sda

--- src/Http/Classes/PaycomApplication.php
+++ src/Http/Classes/PaycomApplication.php
@@ -332,7 +332,7 @@ class PaycomApplication
         }
 
         $order = Order::where('id', $this->request->params['account']['order_id'])->first();
-
+        return $order;
         if (!$order || !$order->id) {
             throw new PaycomException(
                 $this->request->id,
